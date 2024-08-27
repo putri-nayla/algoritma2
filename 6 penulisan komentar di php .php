02c4d30530fe02437cecefda110b18ab416e8b49
putri-nayla <putri20820@gmail.com>
2024-08-27 13:14:35 +0700

    membuat komentar di php

---
+++ 6 penulisan komentar di php .php
@@ -0,0 +1,15 @@
+<?php 
+
+// ini adalah komentar
+echo "Hello world";
+
+/*
+ini adalah komentar 
+yang lebih dari satu 
+baris
+*/
+echo "Kamu sudah dewasa";
+echo "<br> saya sudah makan asap";
+echo "<br> by : putri";
+?>
+    
