93da6ba2022e6d8211392ceb059fcb4ba4bb6a4d
putri-nayla <putri20820@gmail.com>
2024-08-27 11:24:31 +0700

    membuat statement dan expressi

---
+++ 4 menulis statement dan ekpresi .php
@@ -0,0 +1,20 @@
+<?php
+echo "ini statement 1";
+echo "ini statement 2";
+//deklarasi sebuah variable
+$b=15;
+$c=7;
+//ekpresi
+$a1 =  $b * $c;
+$a2 =  $b + $c;
+// cetak ekpresi
+echo "<p> ini ekpresi ->  $a1 = $b * $c <p>" ;
+echo "<p> ini ekpresi ->  $a2 = $b + $c <p>";
+echo " <p> yaitu :" .$a1;
+echo "<p> yaitu :" . $a2;
+echo "<p> by putri nayla </p>";
+echo " ini statement 1";
+echo " ini statement 2";
+
+$a = $b + $c;
+?>
