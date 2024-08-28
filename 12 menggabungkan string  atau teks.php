4c77428b80f894022feb2429442b3854b39770d5
putri-nayla <putri20820@gmail.com>
2024-08-28 08:20:55 +0700

    menggabungkan string teks php

---
+++ 12 menggabungkan string  atau teks.php
@@ -0,0 +1,11 @@
+<?php
+$txt1 = "Tutorial PHP";
+$txt2 = "putri nayla";
+$x = 7;
+$y = 4;
+
+echo "<h2>" . $txt1 . "</h2>";
+echo "Dibelajar pemrograman PHP di " . $txt2 . "<br>";
+echo $x + $y;
+?>
+
